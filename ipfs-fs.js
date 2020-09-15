5dd39eb29bacc02bcbebd2c48fd3664c2d856bcc
Seth Nickell <snickell@gmail.com>
2020-09-14 22:04:57 -1000

    Use async, and inject IPFS into each method

--- ipfs-fs.js
+++ ipfs-fs.js
@@ -1,59 +1,77 @@
+// could also import this.... or....
+const IPFS = window.Ipfs
+
 class FS {
   constructor() {
     this.promises = this
   }
 
-  readFile(path, options) {
+  async initIPFS() {
+    this._ipfs = IPFS.create()
+  }
+
+  async readFile(path, options) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_readfile_path_options
     console.log("readFile", path, options)
   }
 
-  writeFile(file, data, options) {
+  async writeFile(file, data, options) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_writefile_file_data_options
     console.log("writeFile", file, data, options)
   }
 
-  unlink(path) {
+  async unlink(path) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_unlink_path
     console.log("unlink", path)
   }
 
-  readdir(path, options) {
+  async readdir(path, options) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_readdir_path_options
     console.log("readdir", path, options)
   }
 
-  mkdir(path, mode) {
+  async mkdir(path, mode) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_mkdir_path_options
     console.log("mkdir", path, mode)
   }
 
-  rmdir(path) {
+  async rmdir(path) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_rmdir_path
     console.log("rmdir", path)
   }
 
-  stat(path, options) {
+  async stat(path, options) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_stat_path_options
     console.log("stat", path, options)
   }
 
-  lstat(path, options) {
+  async lstat(path, options) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_lstat_path_options
     console.log("lstat", path, options)
   }
 
-  readlink(path, options) {
+  async readlink(path, options) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_readlink_path_options) (optional ¹(#footnote-1)
     console.log("readlink", path, options)
   }
 
-  symlink(target, path, type) {
+  async symlink(target, path, type) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_symlink_target_path_type) (optional ¹(#footnote-1)
     console.log("symlink", target, path, type)
   }
 
-  chmod(path, mode) {
+  async chmod(path, mode) {
+    const ipfs = await this._ipfs
     // https://nodejs.org/api/fs.html#fs_fspromises_chmod_path_mode) (optional ²(#footnote-2)
     console.log("chmod", path, mode)
   }
